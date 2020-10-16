975e45fa234284fcf3230295b4a26b77ace12fff
blagovest doshkov <blagovest.doshkov@tripleplus.io>
2020-10-16 15:04:37 +0300

    Added Web API for GuardPatrol ChangeAvailability

--- SecuritySystem.Web/Features/Guards/GuardPatrolsController.cs
+++ SecuritySystem.Web/Features/Guards/GuardPatrolsController.cs
@@ -3,6 +3,7 @@
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using SecuritySystem.Application.Common;
+    using SecuritySystem.Application.Guards.GuardPatrols.Commands.ChangeAvailability;
     using SecuritySystem.Application.Guards.GuardPatrols.Commands.Create;
     using SecuritySystem.Application.Guards.GuardPatrols.Commands.Delete;
     using SecuritySystem.Application.Guards.GuardPatrols.Commands.Location;
@@ -33,6 +34,12 @@
         [Route(Id + PathSeparator + nameof(UpdateLocation))]
         public async Task<ActionResult> UpdateLocation(
             int id, UpdateLocationCommand commmand)
+            => await this.Send(commmand.SetId(id));
+
+        [HttpPut]
+        [Route(Id + PathSeparator + nameof(ChangeAvailability))]
+        public async Task<ActionResult> ChangeAvailability(
+            int id, ChangeAvailabilityCommand commmand)
             => await this.Send(commmand.SetId(id));
     }
 }
